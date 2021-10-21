6c4e029538c403f3d76ed08947c34370ceb609b1
MauroDeStefano <maurodestefano@gmail.com>
2021-10-21 16:41:53 +0200

    Add controllers for the verify of the content made by the user

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ const newCustomer = parseInt(prompt("Inserisci la tua età."));
 const kmToDo = parseInt(prompt("Dicci quanti km hai intenzione di percorrere."));
 let prizeForAge = 0;
 
+let validValue= "true";
+let errorMsg; 
+let output;
+
+if(isNaN(newCustomer) || isNaN(kmToDo)){
+  validValue = false;
+  errorMsg = 'Inserire un numero valido';
+}
+
+if(newCustomer < 0 || newCustomer > 120){
+  validValue = false;
+  errorMsg = 'Inserire un età credibile';
+}
+
 if(newCustomer < 18){
   prizeForAge = kmPrice * kmToDo * 0.80;
 }else if (newCustomer >= 65){
@@ -16,10 +30,18 @@ prizeForAge = prizeForAge.toFixed(2);
 
 console.log(prizeForAge);
 
-document.getElementById("output").innerHTML = 
+output = 
 `
-
-  ${prizeForAge}
+  <p>Ecco il prezzo che ti abbiamo dedicato!<br>
+  Siccome hai ${newCustomer} anni.<br>
+  Paghi solo
+  <strong>${prizeForAge}<strong>!!
 `
 ;
+
+if(validValue){
+  document.getElementById("output").innerHTML = output;
+}else{
+  document.getElementById("output").innerHTML = errorMsg;
+}
 
